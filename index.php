8b032ea0ab0f93d58fe16f48e893ed7152ca570a
Tobias Reich <tobias.reich.ich@gmail.com>
2014-04-01 17:56:56 +0200

    Removed unused code and changed // to #

--- index.php
+++ index.php
@@ -9,46 +9,34 @@
 
 define('LYCHEE', true);
 
-// Config
-$lychee		= '../': // Path to the root of Lychee
-$album404	= '../'; // When album not found, redirect to this site
-$name404	= '../'; // When name missing, redirect to this site
+# Config
+$lychee		= '../': # Path to the root of Lychee
+$album404	= '../'; # When album not found, redirect to this site
+$name404	= '../'; # When name missing, redirect to this site
 
-// Declare
-$error = '';
-
-// Include
+# Include
 require($lychee . 'data/config.php');
 require($lychee . 'php/modules/db.php');
 require($lychee . 'php/modules/misc.php');
 
-// Connect and get settings
+# Connect and get settings
 $database	= dbConnect();
-$name		= $_GET['code'];
+$name		= mysqli_real_escape_string($database, urldecode($_GET['name']));
 
-// Escape
-foreach(array_keys($_POST) as $key)	$_POST[$key] = mysqli_real_escape_string($database, urldecode($_POST[$key]));
-foreach(array_keys($_GET) as $key)	$_GET[$key] = mysqli_real_escape_string($database, urldecode($_GET[$key]));
-
-// Check code
+# Check code
 if (!isset($name)||$name==='') {
 	header("Location: $name404");
 	exit();
 }
 
-// Search code
+# Search code
 $query	= "SELECT id FROM lychee_albums WHERE title = '$name' LIMIT 1;";
 $result = $database->query($query);
 $return = $result->fetch_array();
 $id		= $return['id'];
 
-// Redirect to album
-if (!isset($return, $id)) {
-	header("Location: $name404");
-	exit();
-} else {
-	header("Location: $lychee#$id");
-	exit();
-}
+# Redirect to album
+if (!isset($return, $id)) header("Location: $name404");
+else header("Location: $lychee#$id");
 
 ?>
